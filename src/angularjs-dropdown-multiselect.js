b3fa8ba90cce40035f33ecb86bc4478343ff927a
mkfsn <p408865@gmail.com>
2017-02-20 17:00:19 +0800

    Prevent infinite loop

--- src/angularjs-dropdown-multiselect.js
+++ src/angularjs-dropdown-multiselect.js
@@ -260,9 +260,8 @@ directiveModule.directive('ngDropdownMultiselect', ['$filter', '$document', '$co
 					if ($scope.settings.smartButtonMaxItems > 0) {
 
 						var paddingWidth = 12 * 2,
-								borderWidth = 1 * 2,
-								dropdownIconWidth = 8;
-						var ellipsisWidth = textWidth("...");
+						    borderWidth = 1 * 2,
+						    dropdownIconWidth = 8;
 						var widthLimit = $element[0].offsetWidth - paddingWidth - borderWidth - dropdownIconWidth;
 
 						var itemsText = [];
@@ -283,8 +282,12 @@ directiveModule.directive('ngDropdownMultiselect', ['$filter', '$document', '$co
 
 						var result = itemsText.join(', ');
 						var index = result.length - 4;
+
+						if ($element[0].offsetWidth === 0)
+							return result;
+
 						while (textWidth(result) > widthLimit) {
-							if (itemsText[itemsText.length - 1] !== "...") {
+							if (itemsText[itemsText.length - 1] !== '...') {
 								itemsText.push('...');
 								result = result + "...";
 							}
